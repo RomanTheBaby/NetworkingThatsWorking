85cae19d988302e9d5bcf667e7878db234aa6a32
RomanTheBaby <mailofrom@gmail.com>
2024-08-10 19:59:41 -0600

    Initial Commit

---
+++ NetworkingThatsWorking/NetworkingThatsWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingThatsWorkingApp.swift
+//  NetworkingThatsWorking
+//
+//  Created by Roman on 2024-08-10.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingThatsWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
